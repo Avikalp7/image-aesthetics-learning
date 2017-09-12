f6257afe8d9a540e171363c5c7858d2426e3661b
Madhav Datt <madhav.datt@hotmail.com>
2017-09-12 22:37:56 +0530

    fix relative path with bias identification

--- src/FeatureExtraction/aspect_ratio.py
+++ src/FeatureExtraction/aspect_ratio.py
@@ -3,8 +3,8 @@ import numpy as np
 
 
 def main():
-    path = "../../Data/photonet_dataset/images/"
-    subset_indices = list(np.load('../../Data/good_indices.npy'))
+    path = "../../data/photonet_dataset/images/"
+    subset_indices = list(np.load('../../data/good_indices.npy'))
     image_sizes = []
 
     for index in subset_indices:
@@ -12,7 +12,7 @@ def main():
         img = Image.open(current_image)
         img = np.array(img)
         image_sizes.append([img.shape[0], img.shape[1]])
-    np.save('../../Data/image_sizes_40p.npy', image_sizes)
+    np.save('../../data/image_sizes_40p.npy', image_sizes)
 
 
 if __name__ == "__main__":
